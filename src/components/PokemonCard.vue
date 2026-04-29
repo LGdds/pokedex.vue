0ddc90d2f30b23a40ae9e54227402ba04f86e2f4
LGdds <guga5378@gmail.com>
2026-04-28 21:23:43 -0300

    final commit

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -24,7 +24,6 @@ const colors = {
 </script>
 
 <template>
-<<<<<<< HEAD
 <router-link :to="'/pokemon/' + pokemon.name" class="card">
     <img :src="pokemon.image">
     <h2>{{ pokemon.name }}</h2>
@@ -33,33 +32,11 @@ const colors = {
             {{ type }}
         </span>
     </div>
-=======
-<router-link
-:to="'/pokemon/' + pokemon.name"
-class="card"
->
-
-<img :src="pokemon.image">
-
-<h2>{{ pokemon.name }}</h2>
-
-<div class="types">
-<span
-v-for="type in pokemon.types"
-:key="type"
-:style="{background: colors[type] || '#555'}"
->
-{{ type }}
-</span>
-</div>
-
->>>>>>> a22f930e744faadf063f0d81b2c1c4bdb4ba0abf
 </router-link>
 </template>
 
 <style scoped>
 .card{
-<<<<<<< HEAD
     background:#1e293b;
     padding:20px;
     border-radius:20px;
@@ -95,42 +72,5 @@ img{
     color:white;
     text-transform:capitalize;
     box-shadow:0 4px 8px rgba(0,0,0,.2);
-=======
-background:#1e293b;
-padding:20px;
-border-radius:20px;
-text-align:center;
-text-decoration:none;
-color:white;
-transition:.3s;
-box-shadow:0 10px 20px rgba(0,0,0,.25);
-}
-
-.card:hover{
-transform:translateY(-10px) scale(1.03);
-background:#334155;
-}
-
-img{
-width:100px;
-}
-
-.types{
-margin-top:15px;
-display:flex;
-justify-content:center;
-gap:8px;
-flex-wrap:wrap;
-}
-
-.types span{
-padding:6px 12px;
-border-radius:20px;
-font-size:13px;
-font-weight:bold;
-color:white;
-text-transform:capitalize;
-box-shadow:0 4px 8px rgba(0,0,0,.2);
->>>>>>> a22f930e744faadf063f0d81b2c1c4bdb4ba0abf
 }
 </style>
